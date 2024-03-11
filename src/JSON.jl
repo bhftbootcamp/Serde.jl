39ee09542dfc2777cfad51e89b7b432d219c94dd
Andreas Hartmann <hartan@7x.de>
2024-03-11 06:13:13 +0000

    fixup! JSON: Simplify module code with `Ext` module.

--- src/JSON.jl
+++ src/JSON.jl
@@ -1,6 +1,6 @@
 import ..Ext
 
-to_json(args...) = Ext.JSON.to_json(args...)
-to_pretty_json(args...) = Ext.JSON.to_pretty_json(args...)
-deser_json(args...) = Ext.JSON.deser_json(args...)
-parse_json(args...) = Ext.JSON.parse_json(args...)
+to_json(args...) = Ext.JSON().to_json(args...)
+to_pretty_json(args...) = Ext.JSON().to_pretty_json(args...)
+deser_json(args...) = Ext.JSON().deser_json(args...)
+parse_json(args...) = Ext.JSON().parse_json(args...)
