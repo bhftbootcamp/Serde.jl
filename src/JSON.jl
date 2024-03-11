7fed765983709549311fd3c4a306a884d9ba3732
Andreas Hartmann <hartan@7x.de>
2024-03-11 06:11:57 +0000

    JSON: Simplify module code with `Ext` module.

--- src/JSON.jl
+++ src/JSON.jl
@@ -1,41 +1,6 @@
-json_error() = error("""
-    to use JSON as format, include the 'JSON' dependency in your project and
-    import it into your environment.""")
-json_module() = Base.get_extension(@__MODULE__(), :SerdeJSONExt)
+import ..Ext
 
-function to_json(args...)
-    ext = json_module()
-    if ext === nothing
-        json_error()
-    else
-        return ext.to_json(args...)
-    end
-end
-
-function to_pretty_json(args...)
-    ext = json_module()
-    if ext === nothing
-        json_error()
-    else
-        return ext.to_pretty_json(args...)
-    end
-end
-
-function deser_json(args...)
-    ext = json_module()
-    if ext === nothing
-        json_error()
-    else
-        return ext.deser_json(args...)
-    end
-end
-
-function parse_json(args...)
-    ext = json_module()
-    if ext === nothing
-        json_error()
-    else
-        return ext.parse_json(args...)
-    end
-end
-
+to_json(args...) = Ext.JSON.to_json(args...)
+to_pretty_json(args...) = Ext.JSON.to_pretty_json(args...)
+deser_json(args...) = Ext.JSON.deser_json(args...)
+parse_json(args...) = Ext.JSON.parse_json(args...)
