19bb0e025914e064dc6f62507ff66a1c16f80507
Andreas Hartmann <hartan@7x.de>
2024-03-12 05:29:22 +0000

    CSV: Document function prototypes in `Serde`
    rather than in the respective extension. This allows exposing all
    user-facing documentation as part of the `Serde` package, while still
    keeping the implementation in the (external) extension.

--- ext/SerdeCSVExt/De.jl
+++ ext/SerdeCSVExt/De.jl
@@ -5,33 +5,6 @@ export deser_csv
 using ..ParCsv
 import Serde.to_deser
 
-"""
-    deser_csv(::Type{T}, x; kw...) -> Vector{T}
-
-Creates a new object of type `T` and fill it with values from CSV formated string `x` (or vector of UInt8).
-
-Keyword arguments `kw` is the same as in [`parse_csv`](@ref).
-
-## Examples
-```julia-repl
-julia> struct Data
-           id::Int64
-           name::String
-           grade::Float64
-       end
-
-julia> csv = \"\"\"
-       "id","name","grade"
-       1,"Fred",78.2
-       2,"Benny",82.0
-       \"\"\";
-
-julia> deser_csv(Data, csv)
-2-element Vector{Data}:
- Data(1, "Fred", 78.2)
- Data(2, "Benny", 82.0)
-```
-"""
 function deser_csv(::Type{T}, x; kw...) where {T}
     return to_deser(Vector{T}, parse_csv(x; kw...))
 end
