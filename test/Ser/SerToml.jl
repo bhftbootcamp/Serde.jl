08b39c7f91dea472f32a6127d8082bc20c4c5cb9
Andreas Hartmann <hartan@7x.de>
2024-03-12 05:42:49 +0000

    test: Restore `TOML` tests.

--- test/Ser/SerToml.jl
+++ test/Ser/SerToml.jl
@@ -1,5 +1,7 @@
 # Ser/SerToml
 
+ext = Serde.Ext.TOML()
+
 @testset verbose = true "SerToml" begin
     @testset "Case №1: Dict to Toml" begin
         exp_kvs = Dict(
@@ -91,8 +93,8 @@
             bar2::Vector{Bar2}
         end
 
-        Serde.SerToml.ser_name(::Type{Fooo}, ::Val{:val}) = :test
-        Serde.SerToml.ser_value(::Type{Fooo}, ::Val{:bar1}, x::Bar1) = 1
+        ext.SerToml.ser_name(::Type{Fooo}, ::Val{:val}) = :test
+        ext.SerToml.ser_value(::Type{Fooo}, ::Val{:bar1}, x::Bar1) = 1
 
         exp_str = """
         test = 100
