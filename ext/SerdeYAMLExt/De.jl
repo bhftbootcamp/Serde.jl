661d6827a01464c5f71fa5124e309cce0deb73aa
Andreas Hartmann <hartan@7x.de>
2024-03-26 17:52:23 +0100

    YAML: Use extension mechanism correctly
    and define all functions to be extended in the "base" module, rather
    than defining new functions inside the extension and making them
    accessible from the base package.

--- ext/SerdeYAMLExt/De.jl
+++ ext/SerdeYAMLExt/De.jl
@@ -1,19 +1,18 @@
 module DeYaml
 
-export deser_yaml
-
 using ..ParYaml
 import Serde.to_deser
+import Serde
 
-function deser_yaml(::Type{T}, x; kw...) where {T}
-    return to_deser(T, parse_yaml(x; kw...))
+function Serde.YAML.deser_yaml(::Type{T}, x; kw...) where {T}
+    return to_deser(T, Serde.YAML.parse_yaml(x; kw...))
 end
 
-deser_yaml(::Type{Nothing}, _) = nothing
-deser_yaml(::Type{Missing}, _) = missing
+Serde.YAML.deser_yaml(::Type{Nothing}, _) = nothing
+Serde.YAML.deser_yaml(::Type{Missing}, _) = missing
 
-function deser_yaml(f::Function, x; kw...)
-    object = parse_yaml(x; kw...)
+function Serde.YAML.deser_yaml(f::Function, x; kw...)
+    object = Serde.YAML.parse_yaml(x; kw...)
     return to_deser(f(object), object)
 end
 
--- ext/SerdeYAMLExt/Par.jl
+++ ext/SerdeYAMLExt/Par.jl
@@ -1,35 +1,19 @@
 module ParYaml
 
-export YamlSyntaxError
-export parse_yaml
-
 using YAML
+import Serde
 
-"""
-    YamlSyntaxError <: Exception
+Base.show(io::IO, e::Serde.YAML.YamlSyntaxError) = print(io, e.message)
 
-Exception thrown when a [`parse_yaml`](@ref) fails due to incorrect YAML syntax or any underlying error that occurs during parsing.
-
-## Fields
-- `message::String`: The error message.
-- `exception::Exception`: The catched exception.
-"""
-struct YamlSyntaxError <: Exception
-    message::String
-    exception::YAML.ParserError
-end
-
-Base.show(io::IO, e::YamlSyntaxError) = print(io, e.message)
-
-function parse_yaml(x::S; dict_type::Type{D} = Dict{String,Any}, kw...) where {S<:AbstractString,D<:AbstractDict}
+function Serde.YAML.parse_yaml(x::S; dict_type::Type{D} = Dict{String,Any}, kw...) where {S<:AbstractString,D<:AbstractDict}
     try
         YAML.load(x; dicttype = dict_type, kw...)
     catch e
-        throw(YamlSyntaxError("invalid YAML syntax", e))
+        throw(Serde.YAML.YamlSyntaxError("invalid YAML syntax", e))
     end
 end
 
-function parse_yaml(x::Vector{UInt8}; kw...)
+function Serde.YAML.parse_yaml(x::Vector{UInt8}; kw...)
     return parse_yaml(unsafe_string(pointer(x), length(x)); kw...)
 end
 
