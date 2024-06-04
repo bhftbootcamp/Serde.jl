7a88e7bc29a037593f385ea35bd75785deca1334
Andreas Hartmann <hartan@7x.de>
2024-06-04 05:56:05 +0000

    Serde: Document new public functions
    `to_string`, `from_string` and `parse`.

--- src/Serde.jl
+++ src/Serde.jl
@@ -83,16 +83,133 @@ function to_symbol(m::Module)::Symbol
     Symbol(join(string.(fullname(m)), '_'))
 end
 
+"""
+    to_string(ext::Module, ...; ...)
+
+Serialize a Julia object to a string.
+
+The first Argument (`ext`) is a Module that is used to dispatch the function call. This means you
+can provide your own methods for arbitrary modules, if desired. The default functions provided with
+`Serde` also use this function internally.
+
+```julia
+julia> using Serde
+
+julia> import YAML
+
+julia> @assert Serde.to_string(YAML, Dict("a" => 1)) == Serde.YAML.to_yaml(Dict("a" => 1))
+```
+
+# Implementation
+
+The actual implementations don't dispatch on the module, but a `Symbol` generated from the `Module`
+argument instead. The [`to_symbol`](@ref) function is used internally to generate the symbols. This
+symbol is then wrapped into a `Val` type to enable dispatch on the symbols constant value.
+
+If you have the module, determine the Symbol like this:
+
+```julia
+julia> import Serde
+
+julia> import MyAwesomeModule
+
+julia> Serde.to_symbol(MyAwesomeModule)
+:MyAwesomeModule
+
+julia> import MyAwesomeModule.MyMarkup
+
+julia> Serde.to_symbol(MyMarkup)
+:MyAwesomeModule_MyMarkup
+```
+
+Then implement your code as follows:
+
+```julia
+using Serde
+
+function Serde.to_string(::Val{:MyAwesomeModule_MyMarkup}, args...; kwargs...)
+    ret = ""
+    # Do some module-specific processing here ...
+    return ret
+end
+```
+
+!!! warning "A hint on type piracy"
+    Since dispatch takes place on symbols rather than real modules, it is possible to define the
+    [`to_string`](@ref), [`from_string`](@ref) and [`parse`](@ref) methods for entirely foreign
+    modules without even importing them. Please [avoid type piracy][1] and prefer to define your
+    own module when adding methods to these functions.
+
+    [1]: https://docs.julialang.org/en/v1/manual/style-guide/#Avoid-type-piracy
+
+If you don't want to spell out the symbol yourself, you can also lean into Julias macros to have
+the correct symbol filled in by the interpreter:
+
+```julia
+using Serde
+import MyAwesomeModule.MyMarkup
+
+@eval function Serde.to_string(::Val{Serde.to_symbol(MyMarkup)}, args...; kwargs...)
+    ret = ""
+    # Do some module-specific processing here ...
+    return ret
+end
+```
+"""
 function to_string end
 function to_string(ext::Module, args...; kwargs...)
     to_string(Val(to_symbol(ext)), args...; kwargs...)
 end
 
+"""
+    from_string(ext::Module, ...; ...)
+
+Deserialize a string to a Julia object.
+
+The first Argument (`ext`) is a Module that is used to dispatch the function call. This means you
+can provide your own methods for arbitrary modules, if desired. The default functions provided with
+`Serde` also use this function internally.
+
+!!! info "Overloading/Adding your own Implementation"
+    To add your own method overloads, refer to the *Implementation* section in [`to_string`](@ref).
+
+```julia
+julia> using Serde
+
+julia> import YAML
+
+julia> val = "a: 1\n"
+
+julia> @assert Serde.from_string(YAML, Dict, val) == Serde.YAML.deser_yaml(Dict, val)
+```
+"""
 function from_string end
 function from_string(ext::Module, args...; kwargs...)
     from_string(Val(to_symbol(ext)), args...; kwargs...)
 end
 
+"""
+    parse(ext::Module, ...; ...)
+
+Parse a string into a Julia `Dict`.
+
+The first Argument (`ext`) is a Module that is used to dispatch the function call. This means you
+can provide your own methods for arbitrary modules, if desired. The default functions provided with
+`Serde` also use this function internally.
+
+!!! info "Overloading/Adding your own Implementation"
+    To add your own method overloads, refer to the *Implementation* section in [`to_string`](@ref).
+
+```julia
+julia> using Serde
+
+julia> import YAML
+
+julia> val = "a: 1\n"
+
+julia> @assert Serde.parse(YAML, val) == Serde.YAML.parse_yaml(val)
+```
+"""
 function parse end
 function parse(ext::Module, args...; kwargs...)
     parse(Val(to_symbol(ext)), args...; kwargs...)
